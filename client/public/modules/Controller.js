c9bba95cdc2e907c1c8aa0b8e1b265a18cc77bdb
leibl <leibl@gmx.at>
2020-12-19 12:23:04 +0100

    posenet loading before game

--- client/public/modules/Controller.js
+++ client/public/modules/Controller.js
@@ -3,11 +3,14 @@ import { Title } from './screens/Title.js';
 import { Game } from './screens/Game.js';
 import { GameOver } from './screens/GameOver.js';
 import { globals } from './globals.js'
+import {BodyParts, PoseDetector} from "./PoseDetector.js";
 
 export class Controller {
     constructor() {
 
         this.activeScreen = new Title();
+        this.poseDetector = null;
+        this.isPosDetectorInitialized = false;
 
         // tmp global cfg
         console.log(`test cfg -- globals.testvar: ${globals.testvar}`)
@@ -16,14 +19,28 @@ export class Controller {
     }
 
     init() {
+        this.initPosenet();
+
         console.log("Controller initialized.")
+    }
+
+    initPosenet() {
+        this.poseDetector = new PoseDetector(this, $("#webcamVideo")[0], 0.3, 11);
+        
+        this.poseDetector.init(globals.useResNet).then(() => {
+            this.isPosDetectorInitialized = true;
+            document.querySelector("#startButton").disabled = false;
+            document.querySelector("#posenet-loading").innerHTML = `
+            <i class="fas fa-thumbs-up"></i>
+            `;
+        });
     }
 
     createScreenControl(screenID) {
         if (screenID === "title") {
             return new Title();
         } else if (screenID === "game") {
-            return new Game();
+            return new Game(this.poseDetector);
         } else if (screenID === "gameover") {
             return new GameOver();
         }
