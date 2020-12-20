62f67214bee9f85a594150fa2c68957b224620d8
Bernd Münzer <bernd@itec.aau.at>
2020-12-20 05:19:03 +0100

    heroku websocket url fix

--- public/modules/globals.js
+++ public/modules/globals.js
@@ -1,6 +1,6 @@
 var globals = {};
 
-globals.websocketURL = "localhost:3000";
+globals.websocketURL = "body-pencil-deluxe-server.herokuapp.com";
 
 globals.selectedColor = "black";
 globals.selectedLineWidth = 5;
